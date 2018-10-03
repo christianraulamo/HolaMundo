4f1bf89b5dd5ba5559550565336122f79b1bfdd6
Christian Raúl Amo Olsson <christianraulamo24@gmail.com>
2018-10-03 10:57:18 +0200

    Proyecto actualizado

--- app/src/main/java/com/example/chris/holamundo/MainActivity.java
+++ app/src/main/java/com/example/chris/holamundo/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.chris.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msg = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg.setText(R.string.texto_pulsado);
+            }
+        });
+
     }
 }
